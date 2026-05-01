cb7a316a8186c57e84a9a8756b6e2cecf6948f05
Junior <eecfredes@gmail.com>
2026-04-30 23:14:09 -0300

    :rocket:.env config

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from core.config import settings
+
 app = FastAPI(
     title="🗡️ Escolha sua proxima aventura",
     description="API para um gerador de RPG aventuras solo.",
@@ -11,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
